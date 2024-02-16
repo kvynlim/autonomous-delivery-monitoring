49b6455a28bfb05690ef3edea4b95e52b3a9773b
kvynlim <kvynlim@ydtautomation.com>
2024-02-16 22:14:56 +0800

    Update main.py

---
+++ api-server/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from os.path import dirname, join, realpath
+from sys import path
+from uvicorn import run
+
+current_path = realpath(__file__)
+path.append(join(dirname(current_path), "src"))
+path.append(join(dirname(dirname(current_path)), "lib", "src"))
+
+from src.fleet_controller import FleetController
+
+app = FastAPI()
+app.include_router(FleetController().get_router())
+
+def main():
+    run("main:app", host="0.0.0.0", port=5000, log_level="info")
+
+if __name__ == "__main__":
+    main()
